239dba6739e0ed62c907059882e85637e7dd90da
Tim Wagner <t.wagner@techdivision.com>
2020-05-22 14:27:29 +0200

    Update todo annotations

--- src/Subjects/OptionSubject.php
+++ src/Subjects/OptionSubject.php
@@ -76,7 +76,8 @@ class OptionSubject extends AbstractAttributeSubject implements OptionSubjectInt
         }
 
         // initialize the flag whether to override images or not
-        // TODO: Create new constant ConfigurationKeys::OVERRIDE_IMAGES for future release
+        // @todo https://github.com/techdivision/import/issues/181
+        //       Create new constant ConfigurationKeys::OVERRIDE_IMAGES for future release
         if ($this->getConfiguration()->hasParam('override-images')) {
             $this->setOverrideImages($this->getConfiguration()->getParam('override-images'));
         }

132dc46fc05b0304adcce5d773a34c632f09e07a
Tim Wagner <t.wagner@techdivision.com>
2021-02-04 16:49:29 +0100

    Add @deprecated annotations for upcoming 22.x branch

--- src/Actions/Processors/AttributeOptionSwatchDeleteProcessor.php
+++ src/Actions/Processors/AttributeOptionSwatchDeleteProcessor.php
@@ -26,11 +26,13 @@ use TechDivision\Import\Actions\Processors\AbstractDeleteProcessor;
 /**
  * The EAV attribute option swatch value delete processor implementation.
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2016 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-attribute
- * @link      http://www.techdivision.com
+ * @author     Tim Wagner <t.wagner@techdivision.com>
+ * @copyright  2021 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/import-attribute
+ * @link       http://www.techdivision.com
+ * @deprecated Since 21.0.1
+ * @see        \TechDivision\Import\Actions\Processors\GenericProcessor
  */
 class AttributeOptionSwatchDeleteProcessor extends AbstractDeleteProcessor
 {
